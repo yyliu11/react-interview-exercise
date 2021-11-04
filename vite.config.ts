c4d89eaad4f5a109637791d5fd1a9a5fdbe46ea3
Ash Martian <programmer.brandon@gmail.com>
2021-11-04 12:05:31 -0700

    Use vite paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,11 @@
 import { defineConfig, Alias } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import * as tsconfig from './tsconfig.paths.json';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-function readAliasFromTsConfig(): Alias[] {
-  const pathReplaceRegex = new RegExp(/\/\*$/, '');
-  return Object.entries(tsconfig.compilerOptions.paths).reduce(
-      (aliases, [fromPaths, toPaths]) => {
-          const find = fromPaths.replace(pathReplaceRegex, '');
-          const toPath = toPaths[0].replace(pathReplaceRegex, '');
-          const replacement = path.resolve(__dirname, toPath);
-          aliases.push({ find, replacement });
-          return aliases;
-      },
-      [] as Alias[],
-  );
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [...readAliasFromTsConfig()] as Alias[]
-  },
+  plugins: [react(), tsconfigPaths()],
   base: "/react-interview-exercise/"
 })
 
