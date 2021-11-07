45ff6c2135a88ecc9f309ba3f52559cf7d095b5b
Emily Liu <emilylau211@gmail.com>
2021-11-06 22:25:56 -0700

    Search results of school districts are displayed.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,9 +14,11 @@ import {
     InputGroup, // Some Chakra components that might be usefull
     HStack,
     VStack,
-    InputRightAddon
+    InputRightAddon,
+    Box,
+    Select
 } from "@chakra-ui/react"
-import { Search2Icon } from '@chakra-ui/icons'
+import { Search2Icon, InfoIcon } from '@chakra-ui/icons'
 import { Card } from '@components/design/Card'
 import { searchSchoolDistricts, searchSchools, NCESDistrictFeatureAttributes, NCESSchoolFeatureAttributes } from "@utils/nces"
 
@@ -26,61 +28,78 @@ const Home: React.FC = () => {
     const [schoolSearch, setSchoolSearch] = useState('')
     const [districtData, setDistrictData] = useState<NCESDistrictFeatureAttributes[]>([]);
     const [schoolData, setSchoolData] = useState<NCESSchoolFeatureAttributes[]>([]);
+    const [showResult, setShowResult] = useState(false);
 
-    const demo = async () => { // see console for api result examples
-
-        const demoDistrictSearch = await searchSchoolDistricts("Peninsula School District")
-        setDistrictSearch(demoDistrictSearch)
-
+    const demo = async () => { // see console for api result example
         const demoSchoolSearch = await searchSchools("m", demoDistrictSearch[1].LEAID)
         setSchoolSearch(demoSchoolSearch)
         console.log("School Example", demoSchoolSearch)
-
     }
 
 
     const handleDistrictSearch = async (dist) => {
-            const districts = await searchSchoolDistricts(dist)
-            setDistrictData(districts)
+            const districts = await searchSchoolDistricts(dist);
+            setDistrictData(districts);
+            setShowResult(true);
     }
     console.log("District example", districtData)
 
     return (
-        <Center bg="tomato" padding="100px" height="auto">
-            <ScaleFade initialScale={0.9} in={true}>
-                <Card variant="rounded" borderColor="blue" height="auto">
-                    <Heading>School Data Finder</Heading>
-                    <InputGroup>
-                        <Input type="text"
-                            width="500px"
-                            placeholder="search school district"
-                            borderRadius="20px"
-                            onChange={e => setDistrictSearch(e.target.value)}
-                        />
-                        <InputRightAddon
-                            backgroundColor="green"
-                            children={<Search2Icon color='white'/>}
-                            borderRadius="20px"
-                            onClick={() => handleDistrictSearch(districtSearch)}
-                        />
-                    </InputGroup>
-                    {/* <Text>
-                        How would you utilize React.useEffect with the searchSchoolDistricts and searchSchools functions? <br />
-                        Using <a href="https://chakra-ui.com/docs/principles" target="_blank">Chakra-UI</a> or your favorite UI toolkit, build an interface that allows the user to: <br />
-                        <OrderedList>
-                            <ListItem>Search for a district</ListItem>
-                            <ListItem>Search for a school within the district (or bypass district filter)</ListItem>
-                            <ListItem>View all returned data in an organized way</ListItem>
-                        </OrderedList>
-                    </Text>
-                    <Divider margin={4} />
-                    <Text>
-                        Check the console for example of returned data. <b>Happy coding!</b>< br />
-                        {searching ? <Spinner /> : <></>}< br />
-                        {districtSearch.length} Demo Districts<br />
-                        {schoolSearch.length} Demo Schools<br />
-                    </Text> */}
-                </Card>
+        <Center bg="tomato" p="110px" h="100%">
+            <ScaleFade initialScale={0.9} in={true} >
+                <Heading textAlign="center" >School Data Finder</Heading>
+                <InputGroup p="20px" justifyContent="center">
+                    <Input
+                        type="text"
+                        width="350px"
+                        placeholder="search school district"
+                        borderRadius="20px"
+                        onChange={e => setDistrictSearch(e.target.value)}
+                    />
+                    <InputRightAddon
+                        backgroundColor="green"
+                        children={<Search2Icon color='white'/>}
+                        borderRadius="20px"
+                        cursor="pointer"
+                        onClick={() => handleDistrictSearch(districtSearch)}
+                    />
+                </InputGroup>
+                {showResult ?
+                    <Card
+                        variant="rounded"
+                        border="transparent"
+                        h="200px"
+                        w="250px"
+                        zIndex="1"
+                        position="absolute"
+                        right="10"
+                        top="25%"
+                    >
+                        <Select defaultValue={districtData[0].NAME} >
+                            {districtData.map((district, idx) =>
+                                <option key={idx} value={district.NAME}>{district.NAME}</option>
+                            )}
+                        </Select>
+                    </Card>: null
+                }
+                {showResult ?
+                    <Card variant="rounded" border="transparent" h="auto" w="700px">
+                        {districtData.map((district, idx) =>
+                            <Box key={idx} p={3} display={{ md: "flex" }} w="95%" borderBottom='2px'>
+                                <Text
+                                    fontWeight="bold"
+                                    fontSize="sm"
+                                    letterSpacing="wide"
+                                    color="teal.600"
+                                    marginLeft="3%"
+                                >
+                                    {district.NAME}
+                                </Text>
+                                <InfoIcon color="teal.600" marginLeft="auto" marginRight="3%" cursor="pointer"/>
+                            </Box>
+                        )}
+                    </Card> : null
+                }
             </ScaleFade>
         </Center>
     );
